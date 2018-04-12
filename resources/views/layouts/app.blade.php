371cee46c20922976f68411dcfa8ce0dfaed0328
iyangankwibowo <iyang_wibowo@apps.ipb.ac.id>
2018-04-12 16:04:15 +0700

    make it better

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title> The Good Place </title>
+    <title> EyeBook </title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
